75038915a7e968588edc67fbfd706f36d45f687f
elsie eunjung <1414207@hcmut.edu.vn>
2017-11-26 20:08:34 +0700

    Setup html/css/js path

--- app/views/explorer.php
+++ app/views/explorer.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
 
+<?php 
+    $base_url = 'http://localhost/CoffeeStoreWeb/public';
+ ?>
+ 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome = 1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <title>Explorer</title>
     <link rel="stylesheet" type="text/css" href="../resources/css/semantic.css">
--- app/views/result.php
+++ app/views/result.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html>
 
+<?php 
+    $base_url = 'http://localhost/CoffeeStoreWeb/public';
+ ?>
+ 
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome = 1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <title>Search Result</title>
-    <link rel="stylesheet" type="text/css" href="../resources/css/semantic.css">
-    <link rel="stylesheet" type="text/css" href="../resources/css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="../resources/css/result.css">
+    <link rel="stylesheet" type="text/css" href="<?= $base_url ?>/css/semantic.css">
+    <link rel="stylesheet" type="text/css" href="<?= $base_url ?>/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="<?= $base_url ?>/css/result.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-    <script type="text/javascript" src="../resources/js/semantic.js"></script>
+    <script type="text/javascript" src="<?= $base_url ?>/js/semantic.js"></script>
 </head>
 
 <body>
