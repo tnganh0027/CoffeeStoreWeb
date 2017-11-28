c690bd023fbb84ff3d177deba4ae5143d39187dc
elsie eunjung <1414207@hcmut.edu.vn>
2017-11-28 10:15:21 +0700

    Upload_Script_DB

--- public/database/cfs_db.sql
+++ public/database/cfs_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2017 at 04:11 AM
+-- Generation Time: Nov 28, 2017 at 04:14 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -19,6 +19,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `cfs_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cfm_data`
+--
+
+CREATE TABLE `cfm_data` (
+  `id` int(11) NOT NULL,
+  `image` text COLLATE utf8_unicode_ci NOT NULL,
+  `about` text COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `idname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `link_fb` text COLLATE utf8_unicode_ci NOT NULL,
+  `link_ins` text COLLATE utf8_unicode_ci NOT NULL,
+  `link_tw` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `cfm_data`
+--
+
+INSERT INTO `cfm_data` (`id`, `image`, `about`, `name`, `idname`, `link_fb`, `link_ins`, `link_tw`) VALUES
+(1, 'http://localhost/CoffeeStoreWeb/public/uploads/mem_1.jpg', '기억하나요 우리 함께 했던 시간 L.O.V.E LUV.', 'Binh Nguyen Khanh', 'Habbangie &hearts;', 'https://www.facebook.com/cray.jjj', '', ''),
+(3, 'http://localhost/CoffeeStoreWeb/public/uploads/mem_3.jpg', '내 몸맨 nice nice body 잘빠진 다리 쌔끈한 허리.', 'Anh Truong Ngoc', 'Hyomin &hearts;', 'https://www.facebook.com/profile.php?id=100010150110239', '', ''),
+(4, 'http://localhost/CoffeeStoreWeb/public/uploads/mem_2.jpg', '너와 나 지금부터 잠시 12345. 눈을 감고 가만히 서 12345.', 'Phuong Do Hoang Minh', 'Mamarong &hearts;', 'https://www.facebook.com/phuong.dohoang07', '', ''),
+(5, 'http://localhost/CoffeeStoreWeb/public/uploads/mem_4.jpg', 'How are you 내 사랑 잃어버린 조각 보고 싶어 울다 지칠 때 난.', 'Tri Le Nguyen Minh', 'Elsie Ej &hearts;', 'https://www.facebook.com/tri.le.7186', '', '');
 
 -- --------------------------------------------------------
 
@@ -49,6 +76,12 @@ INSERT INTO `cfs_data` (`id`, `image`, `name`, `address`, `about`, `view`, `star
 --
 
 --
+-- Indexes for table `cfm_data`
+--
+ALTER TABLE `cfm_data`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `cfs_data`
 --
 ALTER TABLE `cfs_data`
@@ -58,6 +91,11 @@ ALTER TABLE `cfs_data`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `cfm_data`
+--
+ALTER TABLE `cfm_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `cfs_data`
 --
