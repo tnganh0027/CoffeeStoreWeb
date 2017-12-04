98fe4fb76fff82cc5b5dca912d0e97e0a793a43b
elsie eunjung <1414207@hcmut.edu.vn>
2017-12-04 22:26:09 +0700

    config_new_pass

--- app/controllers/Send_mail.php
+++ app/controllers/Send_mail.php
@@ -17,7 +17,7 @@
 			$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 			$mail->SMTPAuth = true;                               // Enable SMTP authentication
 			$mail->Username = 'chinhcuteee0712@gmail.com';                 // SMTP username
-			$mail->Password = 'ahyqfhfrzmticbon';                           // SMTP password
+			$mail->Password = 'eqrwhxryybfzwxey';                           // SMTP password
 			$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 			$mail->Port = 587;                                    // TCP port to connect to
 
